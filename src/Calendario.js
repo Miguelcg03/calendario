0253b9d40ef1775a24754590e9230aed37c8bc95
Badel <badbonsim@gmail.com>
2024-02-14 23:07:10 +0000

    Subida miercoles eliminar y buscar

--- src/Calendario.js
+++ src/Calendario.js
@@ -5,42 +5,41 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import './Calendario.css';
 import esLocale from '@fullcalendar/core/locales/es';
+import { v4 as uuidv4 } from 'uuid';
 
 const Calendario = () => {
     const [events, setEvents] = useState([
         {
+            id: uuidv4(),
             title: 'Evento 1',
             start: '2024-01-28T10:00:00',
             end: '2024-01-28T12:00:00',
             description: 'Descripción del evento 1',
-            color: 'green' // Color personalizado para el evento 2
+            color: 'green'
         },
         {
+            id: uuidv4(),
             title: 'Evento 2',
             start: '2024-01-29T14:00:00',
             end: '2024-01-29T16:00:00',
             description: 'Descripción del evento 2',
-            color: 'blue' // Color personalizado para el evento 2
+            color: 'blue'
         }
     ]);
 
-    const handleEventDrop = (info) => {
-        console.log('Evento arrastrado:', info.event);
-        // Aquí manejar la lógica para guardar los cambios en el evento
-    };
-
-    const handleEventResize = (info) => {
-        console.log('Evento redimensionado:', info.event);
-        // Aquí  manejar la lógica para guardar los cambios en el evento
-    };
+    const [searchTerm, setSearchTerm] = useState("");
 
     const handleEventClick = (info) => {
         const title = prompt('Editar título:', info.event.title);
         const description = prompt('Editar descripción:', info.event.extendedProps.description);
         if (title !== null && description !== null) {
-            info.event.setProp('title', title);
-            info.event.setExtendedProp('description', description);
-            // Aquí manejar la lógica para guardar los cambios en el evento
+            const updatedEvents = events.map(event => {
+                if (event.id === info.event.id) {
+                    return { ...event, title, description };
+                }
+                return event;
+            });
+            setEvents(updatedEvents);
         }
     };
 
@@ -48,32 +47,56 @@ const Calendario = () => {
         const title = prompt('Agregar título:');
         const description = prompt('Agregar descripción:');
         if (title !== null && description !== null) {
-            setEvents([
-                ...events,
-                { title, start: info.dateStr, allDay: true, description }
-            ]);
-            // Aquí  manejar la lógica para guardar el nuevo evento
+            const newEvent = {
+                id: uuidv4(),
+                title,
+                start: info.dateStr,
+                allDay: true,
+                description
+            };
+            setEvents(prevEvents => [...prevEvents, newEvent]);
         }
     };
 
+    const handleDeleteEvent = (eventId) => {
+        setEvents(prevEvents => prevEvents.filter(event => event.id !== eventId));
+    };
+
+    const filteredEvents = events.filter(event =>
+        event.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
-        <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            initialView="timeGridWeek"
-            locale={esLocale}
-            firstDay={1} // 0: Domingo, 1: Lunes, 2: Martes, etc.
-            headerToolbar={{
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay'
-            }}
-            editable={true}
-            events={events}
-            eventDrop={handleEventDrop}
-            eventResize={handleEventResize}
-            eventClick={handleEventClick}
-            dateClick={handleDateClick}
-        />
+        <div>
+            <input className="search-input"
+                type="text"
+                placeholder="Buscar evento"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                initialView="timeGridWeek"
+                locale={esLocale}
+                firstDay={1}
+                headerToolbar={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                }}
+                editable={true}
+                events={filteredEvents}
+                eventClick={handleEventClick}
+                dateClick={handleDateClick}
+                eventContent={(arg) => (
+                    <div className="event-container">
+                        <div>{arg.timeText}</div>
+                        <div>{arg.event.title}</div>
+                        <button className="delete-button" onClick={() => handleDeleteEvent(arg.event.id)}>X</button>
+                    </div>
+                )}
+            />
+        </div>
     );
 };
 
