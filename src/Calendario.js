e979aa9c30697b5b542c0ca065b1cd618fc6e5bd
Badel <badbonsim@gmail.com>
2024-02-16 22:29:22 +0000

    Subida de comentarios

--- src/Calendario.js
+++ src/Calendario.js
@@ -1,3 +1,4 @@
+// Importaciones de módulos necesarios
 import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
@@ -7,7 +8,9 @@ import './Calendario.css';
 import esLocale from '@fullcalendar/core/locales/es';
 import { v4 as uuidv4 } from 'uuid';
 
+// Componente principal del calendario
 const Calendario = () => {
+    // Estado para almacenar los eventos del calendario
     const [events, setEvents] = useState([
         {
             id: uuidv4(),
@@ -27,18 +30,22 @@ const Calendario = () => {
         }
     ]);
 
+    // Estado para el término de búsqueda
     const [searchTerm, setSearchTerm] = useState("");
+    // Estado para el evento seleccionado
     const [selectedEvent, setSelectedEvent] = useState(null);
 
+    // Manejador de eventos al hacer clic en un evento del calendario
     const handleEventClick = (info) => {
         setSelectedEvent(info.event);
     };
 
-    
-
+    // Manejador de eventos al hacer clic en una fecha del calendario
     const handleDateClick = (info) => {
+        // Solicitamos al usuario que ingrese el título y la descripción del evento
         const title = prompt('Agregar título:');
         const description = prompt('Agregar descripción:');
+        // Si el usuario proporciona información, creamos un nuevo evento y lo agregamos al estado
         if (title !== null && description !== null) {
             const newEvent = {
                 id: uuidv4(),
@@ -46,38 +53,45 @@ const Calendario = () => {
                 start: info.dateStr,
                 allDay: true,
                 description,
-                color: getRandomColor() // Asignar un color aleatorio al nuevo evento
+                color: getRandomColor()
             };
             setEvents(prevEvents => [...prevEvents, newEvent]);
         }
     };
 
+    // Manejador de eventos para eliminar un evento del calendario
     const handleDeleteEvent = (eventId) => {
         setEvents(prevEvents => prevEvents.filter(event => event.id !== eventId));
     };
 
+    // Filtramos los eventos según el término de búsqueda
     const filteredEvents = events.filter(event =>
         event.title.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
+    // Función para obtener un color aleatorio
     const getRandomColor = () => {
         const colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966'];
         return colors[Math.floor(Math.random() * colors.length)];
     };
+
+    // Retornamos la interfaz del calendario
     return (
         <div>
+            {/* Input para buscar eventos */}
             <input className="search-input"
                 type="text"
                 placeholder="Buscar evento"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
             />
-             <input
-            type="color"
-            style={{ visibility: "hidden" }}
-            value={selectedEvent ? selectedEvent.color : "#000000"}
-        />
-            {/* Remove the input for the color picker */}
+            {/* Input para seleccionar color (oculto) */}
+            <input
+                type="color"
+                style={{ visibility: "hidden" }}
+                value={selectedEvent ? selectedEvent.color : "#000000"}
+            />
+            {/* Componente FullCalendar con sus propiedades y eventos */}
             <FullCalendar
                 plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
                 initialView="timeGridWeek"
